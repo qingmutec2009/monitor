822f6cc5bf6f08af278c02d32aafd7dd85f0149d
dengcaijie <274525766@qq.com>
2022-05-15 20:38:04 +0800

    修改测试类

--- src/qmmonitor/extra/abstracts/AbstractJob.php
+++ src/qmmonitor/extra/abstracts/AbstractJob.php
@@ -3,6 +3,8 @@
 
 namespace qmmonitor\extra\abstracts;
 
+use qmmonitor\extra\pojo\JobArguments;
+
 /**
  * 工作基类
  * Class AbstractJob
@@ -10,6 +12,70 @@ namespace qmmonitor\extra\abstracts;
  */
 abstract class AbstractJob
 {
+    /**
+     * 参数
+     * @var array
+     */
+    protected $params = '';
+
+    /**
+     * @var \qmmonitor\extra\pojo\JobArguments
+     */
+    protected $jobArguments;
+
+    public function __costruct(?JobArguments $jobArguments)
+    {
+        $this->initAttr($jobArguments);
+    }
+    /**
+     * 执行
+     */
+    public final function perform(?JobArguments $jobArguments = null)
+    {
+        $this->initAttr($jobArguments);
+        //注册、兼容旧版本
+        $this->register($this->params);
+        //初始化
+        $this->initialize();
+        //执行
+        $this->handle();
+    }
+
+    /**
+     * 初始化属性
+     * @param JobArguments|null $jobArguments
+     */
+    private function initAttr(?JobArguments $jobArguments)
+    {
+        if (!is_null($jobArguments)) {
+            $this->params = $jobArguments->getMessage();
+            $this->jobArguments = $jobArguments;
+        }
+    }
+
+    /**
+     * 框架日记
+     *
+     * @param $message
+     * @param $type
+     */
+    protected function jobLog(string $message,$type)
+    {
+        $path   = MONITOR_LOG_DIR.date('Y-m-d').'-'.$type.'.log';
+        $string = $message."\n";
+        file_put_contents($path,$string,FILE_APPEND);
+    }
+
+    /**
+     * 队列日志
+     * @param string $message
+     */
+    protected function infoLog(string $message)
+    {
+        $path   = MONITOR_LOG_DIR.date('Y-m-d').$this->jobArguments->getQueueName().'.log';
+        $string = date('Y-m-d H:i:s').' '.$message."\n";
+        file_put_contents($path, $string, FILE_APPEND);
+    }
     /**
      * 注册参数
      * 注册、兼容旧版本
--- src/qmmonitor/test/TestJob.php
+++ src/qmmonitor/test/TestJob.php
@@ -12,70 +12,7 @@ use think\facade\Db;
  */
 class TestJob extends AbstractJob
 {
-    /**
-     * 参数
-     * @var array
-     */
-    protected $params = '';
 
-    /**
-     * @var \qmmonitor\extra\pojo\JobArguments
-     */
-    protected $jobArguments;
-
-    public function __costruct(?JobArguments $jobArguments)
-    {
-        $this->initAttr($jobArguments);
-    }
-    /**
-     * 执行
-     */
-    public final function perform(?JobArguments $jobArguments = null)
-    {
-        $this->initAttr($jobArguments);
-        //注册、兼容旧版本
-        $this->register($this->params);
-        //初始化
-        $this->initialize();
-        //执行
-        $this->handle();
-    }
-
-    /**
-     * 初始化属性
-     * @param JobArguments|null $jobArguments
-     */
-    private function initAttr(?JobArguments $jobArguments)
-    {
-        if (!is_null($jobArguments)) {
-            $this->params = $jobArguments->getMessage();
-            $this->jobArguments = $jobArguments;
-        }
-    }
-
-    /**
-     * 框架日记
-     *
-     * @param $message
-     * @param $type
-     */
-    protected function jobLog(string $message,$type)
-    {
-        $path   = MONITOR_LOG_DIR.date('Y-m-d').'-'.$type.'.log';
-        $string = $message."\n";
-        file_put_contents($path,$string,FILE_APPEND);
-    }
-
-    /**
-     * 队列日志
-     * @param string $message
-     */
-    protected function infoLog(string $message)
-    {
-        $path   = MONITOR_LOG_DIR.date('Y-m-d').$this->jobArguments->getQueueName().'.log';
-        $string = date('Y-m-d H:i:s').' '.$message."\n";
-        file_put_contents($path, $string, FILE_APPEND);
-    }
 
     public function register($param)
     {
