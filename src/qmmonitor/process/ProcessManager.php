c104a69ec8c1d915c0b69f2d4581fa439e8b0ca9
dengcaijie <274525766@qq.com>
2022-05-16 21:00:12 +0800

    去除循环监听信号部分

--- src/qmmonitor/process/ProcessManager.php
+++ src/qmmonitor/process/ProcessManager.php
@@ -33,20 +33,18 @@ class ProcessManager extends AbstractProcess
         $pm = $this->process;
         $pm->addBatch($nowQueueConfig['count'],function (Pool $pool,$workerId) use ($queueName,$amqpConfig,$nowQueueConfig){
             //防止异常退出
-            $running = true;
+            //$running = true;
             $pid = posix_getpid();
             //$this->signal($running, $pool);
             $processName = "php-work-{$queueName}-{$workerId}";
             $this->setProcessName("php-work-{$queueName}-{$workerId}");
             echo("[Worker:{$workerId}] WorkerStarted, pid: {$pid},process:$processName" . "\n");
-            while ($running) {
-                //创建回调-todo $enableCoroutine只是用来临时测试
-                $callBack = RabbitMqManager::getInstance($amqpConfig)
-                    ->consumerCallBack($nowQueueConfig,$queueName,$workerId,$pid);
-                //设置channel并消费
-                $channel = RabbitMqManager::getInstance()->qos();
-                RabbitMqManager::getInstance()->consumer($queueName, $callBack,$channel);
-            }
+            //创建回调-todo $enableCoroutine只是用来临时测试
+            $callBack = RabbitMqManager::getInstance($amqpConfig)
+                ->consumerCallBack($nowQueueConfig,$queueName,$workerId,$pid);
+            //设置channel并消费
+            $channel = RabbitMqManager::getInstance()->qos();
+            RabbitMqManager::getInstance()->consumer($queueName, $callBack,$channel);
         },$enableCoroutine);
         $this->process = $pm;
         return $this->process;
